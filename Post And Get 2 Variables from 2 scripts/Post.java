3bc73d7dffe73371b4e5f40bb11be8af4ed73b45
New Stranger <95246773+MAE-NW@users.noreply.github.com>
2022-06-30 10:11:57 +0100

    Create Post.java

---
+++ Post And Get 2 Variables from 2 scripts/Post.java
@@ -0,0 +1,30 @@
+package JAVARuntime;
+
+import java.util.*;
+import java.text.*;
+import java.net.*;
+import java.math.*;
+import java.io.*;
+import java.nio.*;
+
+
+public class Post extends Component { 
+public float var = 20;
+public float v2 = 99;
+
+
+    @Override
+    public void start() {
+        
+    }
+
+    @Override
+    public void repeat() {
+       
+    }
+
+    @Override
+    public void disabledRepeat() {
+        
+    }
+}
