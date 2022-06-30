cb553270d9fb304a3f32a981f6015447dcda576d
New Stranger <95246773+MAE-NW@users.noreply.github.com>
2022-06-30 10:06:27 +0100

    Update deleteObject.java

--- deleteObject.java
+++ deleteObject.java
@@ -1,6 +1,5 @@
 package JAVARuntime;
 
-// Useful imports
 import java.util.*;
 import java.text.*;
 import java.net.*;
@@ -8,24 +7,18 @@ import java.math.*;
 import java.io.*;
 import java.nio.*;
 
-/**
- * @Author 
-*/
 public class deleteObject extends Component { 
 
-    /// Run only once
     @Override
     public void start() {
         myObject.destroy();
     }
 
-    /// Repeat every frame
     @Override
     public void repeat() {
        
     }
 
-    /// Repeat every frame when component or object is disabled
     @Override
     public void disabledRepeat() {
         
