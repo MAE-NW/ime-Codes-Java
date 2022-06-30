d6ef8c1c0c32181701d4fc138bbf0f0b4ea99dca
New Stranger <95246773+MAE-NW@users.noreply.github.com>
2022-06-30 10:37:53 +0100

    Update fps.java

--- fps.java
+++ fps.java
@@ -1,6 +1,6 @@
 package JAVARuntime;
 
-// Useful imports
+
 import java.util.*;
 import java.text.*;
 import java.net.*;
@@ -8,36 +8,33 @@ import java.math.*;
 import java.io.*;
 import java.nio.*;
 
-/**
- * @Author 
-*/
+
+
 public class fps extends Component { 
-private SUIText a;
-private String b;
-private int c;
-private long d;
-private long e;
-    /// Run only once
+private SUIText amae1;
+private String bmae2;
+private int cmae3;
+private long dmae4;
+private long emae5;
+
+
     @Override
     public void start() {
-        a =(SUIText) myObject.findComponent("SUIText");
+        amae1 =(SUIText) myObject.findComponent("SUIText");
     }
-
-    /// Repeat every frame
     @Override
     public void repeat() {
-        c++;
-        d=System.currentTimeMillis();
-       if(d >= e){
-           e= d+1000;
-           b= (""+c);
-           c = 0;
-           a.setText("FPS :"+b);
+        cmae3++;
+        dmae4=System.currentTimeMillis();
+       if(dmae4 >= emae5){
+           emae5= dmae4+1000;
+           bmae2= (""+cmae3);
+           cmae3 = 0;
+           amae1.setText("FPS :"+bmae2);
        }
         
     }
 
-    /// Repeat every frame when component or object is disabled
     @Override
     public void disabledRepeat() {
         
