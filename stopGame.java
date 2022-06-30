995610691b33928e3a70d01584f4789abc06c322
New Stranger <95246773+MAE-NW@users.noreply.github.com>
2022-06-30 09:54:39 +0100

    Update stopGame.java

--- stopGame.java
+++ stopGame.java
@@ -9,13 +9,13 @@ import java.nio.*;
 
 
 public class stopGame extends Component {
-    public float sp;
+    public float mae;
     @Override
     public void start() {
       //sp = speed game 
       //1.0 = normal speed
       //0.0 = stop game
-      Time.setTimeSpeed(sp);
+      Time.setTimeSpeed(mae);
       
     }
     @Override
