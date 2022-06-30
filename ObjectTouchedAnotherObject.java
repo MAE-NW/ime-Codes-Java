093505ea096dfe6db466f3da7232d82b1fe947df
New Stranger <95246773+MAE-NW@users.noreply.github.com>
2022-06-30 10:23:35 +0100

    Update ObjectTouchedAnotherObject.java

--- ObjectTouchedAnotherObject.java
+++ ObjectTouchedAnotherObject.java
@@ -1,6 +1,6 @@
 package JAVARuntime;
 
-// Useful imports
+
 import java.util.*;
 import java.text.*;
 import java.net.*;
@@ -8,29 +8,23 @@ import java.math.*;
 import java.io.*;
 import java.nio.*;
 
-/**
- * @Author 
-*/
 public class ObjectTouchedAnotherObject extends Component { 
-private Rigidbody rb = new Rigidbody();
-public String anotherobjectname;
-    /// Run only once
+private Rigidbody rbmae = new Rigidbody();
+public String anotherobjectnamemae;
+
     @Override
     public void start() {
-        rb = (Rigidbody)myObject.getPhysics().getPhysicsEntity();
+        rbmae = (Rigidbody)myObject.getPhysics().getPhysicsEntity();
     }
-    /// Repeat every frame
     @Override
     public void repeat() {
         //another object name = name of the ground or other...
-        if (rb.colliderWithName(anotherobjectname)) {
-            Console.log("hhh");
+        if (rbmae.colliderWithName(anotherobjectnamemae)) {
+            Console.log("touched");
 }else{
     
 }
     }
-
-    /// Repeat every frame when component or object is disabled
     @Override
     public void disabledRepeat() {
         
