6f2661d078bf1f6c3da9218f80f6c959ee550512
New Stranger <95246773+MAE-NW@users.noreply.github.com>
2022-06-30 10:41:10 +0100

    Update ChangeWorlds.java

--- ChangeWorlds.java
+++ ChangeWorlds.java
@@ -1,6 +1,5 @@
 package JAVARuntime;
 
-// Useful imports
 import java.util.*;
 import java.text.*;
 import java.net.*;
@@ -8,27 +7,20 @@ import java.math.*;
 import java.io.*;
 import java.nio.*;
 
-/**
- * @Author 
-*/
+
 public class ChangeWorlds extends Component { 
 public PFile World;
-    /// Run only once
+
     @Override
     public void start() {
-        
+        WorldController.loadWorld(World);
     }
+    
 
-    /// Repeat every frame
     @Override
     public void repeat() {
-       if (Input.getKey("start").isUp()){
-           
-           WorldController.loadWorld(World);
-       }
     }
-
-    /// Repeat every frame when component or object is disabled
+   
     @Override
     public void disabledRepeat() {
         
