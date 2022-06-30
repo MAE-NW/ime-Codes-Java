38858911d1825cc7656a287145575b25dc8ae6b9
New Stranger <95246773+MAE-NW@users.noreply.github.com>
2022-06-30 09:52:15 +0100

    Add files via upload

---
+++ speed.java
@@ -0,0 +1,29 @@
+package JAVARuntime;
+
+// Useful imports
+import java.util.*;
+import java.text.*;
+import java.net.*;
+import java.math.*;
+import java.io.*;
+import java.nio.*;
+
+/**
+ * @Author 
+*/
+public class speed extends Component { 
+public float speed;
+    @Override
+    public void start() {
+        
+    }
+    @Override
+    public void repeat() {
+        myObject.getTransform().moveInSeconds(0,0,speed);
+    }
+
+    @Override
+    public void disabledRepeat() {
+        
+    }
+}
